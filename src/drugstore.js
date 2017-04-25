e6d8bc7652bb8067ab76458028caf471d1a91b62
Anton Artemov <Anton Artemov>
2017-04-25 21:24:36 +0300

    Updated server

--- src/drugstore.js
+++ src/drugstore.js
@@ -6,11 +6,8 @@ var async = require('async');
 var express = require('express');
 var pretty = require('express-prettify');
 
-var db = new sqlite3.Database('DrugStores.db');
+var db = new sqlite3.Database('DrugStoreV2.db');
 var restapi = express();
-
-var drugStore = "DrugStore.id, DrugStore.name, DrugStore.work_time, Location.longitude, " +
-    "Location.latitude, Location.address";
 
 restapi.use(pretty({query: 'pretty'}));
 
@@ -19,64 +16,81 @@ function arePointsNear(checkPoint, centerPoint, km) {
     var dLon = (checkPoint.lng - centerPoint.lng) * Math.PI / 180;
     var a = 0.5 - Math.cos(dLat) / 2 + Math.cos(checkPoint.lat * Math.PI / 180)
         * Math.cos(centerPoint.lat * Math.PI / 180) * (1 - Math.cos(dLon)) / 2;
-    d = Math.round(6371000 * 2 * Math.asin(Math.sqrt(a)));
-    return d <= km*1000;
+    var d = Math.round(6371000 * 2 * Math.asin(Math.sqrt(a)));
+
+    return d <= km * 1000;
 }
 
 restapi.route("/drugstores")
     .get(function (req, res) {
+
+        var response = {};
+        response.data = [];
+        response.messege = "";
+
         console.log("start get  info about Drug Store for search");
+
         var drugStoreName;
         var numPage;
 
         if (res.req.query.num_page)
             numPage = res.req.query.num_page;
+
         var num = 20 * (numPage - 1);
+
         if (res.req.query.drug_store_name)
             drugStoreName = res.req.query.drug_store_name;
-        var sqlRequest = "select DrugStore.id, " + drugStore + " from DrugStore " +
-            "left join Location on Location.id = DrugStore.location_id ";
+
+        var sqlRequest = "select * from DrugStore ";
+
         if (drugStoreName)
             sqlRequest += " where DrugStore.name like '" + drugStoreName + "'";
+
         if (numPage == 1)
             sqlRequest += ' limit 0,20';
         else {
             if (numPage > 1)
                 sqlRequest += ' limit ' + num + ',20';
         }
+
         db.all(sqlRequest, function (err, row) {
-            res.json({"data": row})
+            response.data = row;
+            res.json(response)
         });
+
         console.log("finish get  info about Drug Store for search");
     });
 
-restapi.route("/drugstoresmap")
+restapi.route("/drugstores/map/")
     .get(function (req, res) {
 
+        var response = {};
+        response.data = [];
+        response.messege = "";
+
         var center;
+
         if (res.req.query.latitude && res.req.query.longitude)
             center = {lat: parseFloat(res.req.query.latitude), lng: parseFloat(res.req.query.longitude)};
+        else {
+            response.messege = "Empty required params";
+            res.json(response);
+            return
+        }
 
         var result = [];
-        var response={};
-        response.data=[];
 
         async.series([
             function (callback) { // Full info
-                db.all('Select ' + drugStore + ' from DrugStore ' +
-                    'left join Location on Location.id = DrugStore.location_id', function (err, row) {
-                    result =  row;
+                db.all('Select * from DrugStore', function (err, row) {
+                    result = row;
                     callback();
                 })
             },
             function (callback) {
                 for (var position in result)
-                    if (arePointsNear({lat: result[position].longitude, lng: result[position].latitude}, center, 1)){
+                    if (arePointsNear({lat: result[position].latitude, lng: result[position].longitude}, center, 1)) {
                         var drugStoreModel = result[position];
-
-                        db.all("Select * from Phone where Phone.drugstore_id = "+ drugStoreModel.id , function (err, row) {
-                            drugStoreModel.phone = row;
-                        });
 
                         response.data.push(drugStoreModel);
                     }
@@ -89,8 +103,9 @@ restapi.route("/drugstoresmap")
 
 restapi.route('*')
     .get(function (req, res) {
-        var error = {};
-        error.messege = "Route not found";
-        res.json(error)
+        var response = {};
+        response.data = [];
+        response.messege = "Route not found";
+        res.json(response)
     });
 restapi.listen(3000);
